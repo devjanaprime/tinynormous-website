e1e5a26f6af6e2838c51c85bba614dbc7b59a415
Dev Jana <dev@primeacademy.io>
2024-11-10 00:43:54 -0600

    rc1

--- scripts/client.js
+++ scripts/client.js
@@ -1,51 +1,40 @@
 sendEmail=()=>{
-    const objectToSend = {
-        name: document.getElementById( 'nameIn' ).value,
-        email: document.getElementById( 'emailIn' ).value,
-        subject: document.getElementById( 'subjectIn' ).value,
-        content: document.getElementById( 'contentIn' ).value
-    }
-    console.log( 'email:', objectToSend );
+    var link = "mailto:6tuningpegs@gmail.com"
+             + "?cc="+encodeURIComponent(document.getElementById( 'emailIn' ).value)
+             + "&subject=" + encodeURIComponent(document.getElementById( 'subjectIn' ).value)
+             + "&body=from:" + encodeURIComponent(document.getElementById('nameIn').value) 
+             + ": " + encodeURIComponent(document.getElementById('contentIn').value)
+    ;
+    window.location.href = link;
 }
 
 const setUp=()=>{
-    let videosText = ` <div class="w3-col l3 m6 w3-margin-bottom">
-      <a href="http://google.com"><div class="w3-display-container">
-        <div class="w3-display-topleft w3-black w3-padding">Summer House</div>
-        <img src="https://www.w3schools.com/w3images/house5.jpg" alt="House" style="width:100%">
-      </div></a>
-    </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <div class="w3-display-container">
-        <div class="w3-display-topleft w3-black w3-padding">Brick House</div>
-        <img src="https://www.w3schools.com/w3images/house2.jpg" alt="House" style="width:100%">
-      </div>
-    </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <div class="w3-display-container">
-        <div class="w3-display-topleft w3-black w3-padding">Renovated</div>
-        <img src="https://www.w3schools.com/w3images/house3.jpg" alt="House" style="width:100%">
-      </div>
-    </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <div class="w3-display-container">
-        <div class="w3-display-topleft w3-black w3-padding">Barn House</div>
-        <img src="https://www.w3schools.com/w3images/house4.jpg" alt="House" style="width:100%">
-      </div>
-    </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <div class="w3-display-container">
-        <div class="w3-display-topleft w3-black w3-padding">Renovated</div>
-        <img src="https://www.w3schools.com/w3images/house3.jpg" alt="House" style="width:100%">
-      </div>
-    </div>
-    <div class="w3-col l3 m6 w3-margin-bottom">
-      <div class="w3-display-container">
-        <div class="w3-display-topleft w3-black w3-padding">Barn House</div>
-        <img src="https://www.w3schools.com/w3images/house4.jpg" alt="House" style="width:100%">
-      </div>
-    </div>`;
+    shuffle( vids );
+    let videosText = '';
+    for( vid of vids ){
+        videosText += `<div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+          <iframe src="https://www.youtube.com/embed/${vid.url.split( '?v=' )[1]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+      </div>`;
+    }
     document.getElementById( 'videosOut' ).innerHTML=videosText;
+}
+
+const shuffle=(array)=>{
+    let currentIndex = array.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
 }
 
 setUp();
---
+++ scripts/vids.js
@@ -0,0 +1,82 @@
+let vids=[
+    {
+        "title": "Sleepless Not Restless (album Version)",
+        "url": "https://www.youtube.com/watch?v=dUOG8LapAGg"
+    },
+    {
+        "title": "Precipice Recipe (official music video)",
+        "url": "https://www.youtube.com/watch?v=RKLTlnfg_MA"
+    },
+    {
+        "title": "What's Happenin' Wednesday: New Album Announcement, losing Archive.org",
+        "url": "https://www.youtube.com/watch?v=ufB5alGJDdE"
+    },
+    {
+        "title": "Achievement Unlocked!",
+        "url": "https://www.youtube.com/watch?v=ZIm3Vx2aAc8"
+    },
+    {
+        "title": "Rhythm Challenge Crushed on first try!",
+        "url": "https://www.youtube.com/watch?v=By8j4rA0lw4"
+    },
+    {
+        "title": "Hats Livestream (4K upscaled, remastered audio)",
+        "url": "https://www.youtube.com/watch?v=B_G6yfYnRzM"
+    },
+    {
+        "title": "Sleepless Not Restless (electric guitar duet)",
+        "url": "https://www.youtube.com/watch?v=_EMpLJPQLKg"
+    },
+    {
+        "title": "Presque Isle Sunset 4/27/2024",
+        "url": "https://www.youtube.com/watch?v=H9WBomfgnCY"
+    },
+    {
+        "title": "Arrow-tone Sessions EP (full video)",
+        "url": "https://www.youtube.com/watch?v=OSCtyusIIOQ"
+    },
+    {
+        "title": "Loop Sketch 00: Squid Egg (full)",
+        "url": "https://www.youtube.com/watch?v=0DfO-0Ik86c"
+    },
+    {
+        "title": "Belonging (demo)",
+        "url": "https://www.youtube.com/watch?v=HxTHPCm-3AI"
+    },
+    {
+        "title": "Livestream 01: Sweaters (4k upscaled)",
+        "url": "https://www.youtube.com/watch?v=XQE8jexX7FU"
+    },
+    {
+        "title": "4 Track as an Instrument",
+        "url": "https://www.youtube.com/watch?v=7rsPgn4Dn1s"
+    },
+    {
+        "title": "Belonging (a tribute to our pets)",
+        "url": "https://www.youtube.com/watch?v=vrvH6qpuzkU"
+    },
+    {
+        "title": "Fearless Moments",
+        "url": "https://www.youtube.com/watch?v=JZBb9GSAXtY"
+    },
+    {
+        "title": "4 Track Tape Reverse & Reverse Reverb: How They Work & a Little Experiment",
+        "url": "https://www.youtube.com/watch?v=b2dw_4ZBV3w"
+    },
+    {
+        "title": "Don't Give Up The Ship (performance music video)",
+        "url": "https://www.youtube.com/watch?v=eHVhfZk7GE0"
+    },
+    {
+        "title": "Sighing Cyclops (a love letter to early Fall)",
+        "url": "https://www.youtube.com/watch?v=UIamv_r2F3w"
+    },
+    {
+        "title": "Another Only (a love letter to Summer in Minneapolis)",
+        "url": "https://www.youtube.com/watch?v=1muY3WzicP8"
+    },
+    {
+        "title": "First Next Step (Rockford IL, Elkhorn WI, & Eau Claire WI)",
+        "url": "https://www.youtube.com/watch?v=U7VwZsuYuX0"
+    }
+]
